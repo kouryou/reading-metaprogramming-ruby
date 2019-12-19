6059987e5a27deece1c31dff90286e703689266b
kouryou <23131663+kouryou@users.noreply.github.com>
2019-12-19 15:51:08 +0900

    solve 03_method/define

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -2,6 +2,12 @@
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
 
+class A1
+  define_method('//') do
+    '//'
+  end
+end
+
 # Q2.
 # 次の動作をする A2 class を実装する
 # - 1. "SmartHR Dev Team"と返すdev_teamメソッドが存在すること
@@ -9,8 +15,47 @@
 # - 2で定義するメソッドは下記とする
 #   - 受け取った引数の回数分、メソッド名を繰り返した文字列を返すこと
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
+class A2
+  def initialize(args)
+    args.each do |arg|
+      method = "hoge_#{arg}"
+      define_singleton_method(method) do |my_arg|
+        if my_arg
+          method * my_arg
+        else
+          dev_team
+        end
+      end
+    end
+  end
+
+  def dev_team
+    'SmartHR Dev Team'
+  end
+end
 
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+module OriginalAccessor
+  def self.included(klass)
+    klass.define_singleton_method :my_attr_accessor do |*args|
+      args.each do |arg|
+        define_method("#{arg}=") do |my_arg|
+          @arg = my_arg
+
+          if my_arg.is_a?(FalseClass) || my_arg.is_a?(TrueClass)
+            define_singleton_method("#{arg}?") do
+              @arg
+            end
+          end
+        end
+
+        define_method(arg) do
+          @arg
+        end
+      end
+    end
+  end
+end
