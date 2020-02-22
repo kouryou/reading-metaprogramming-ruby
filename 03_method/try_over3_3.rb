f9d5e02651581940264c4b405c72cf3cf1d8748a
kouryou <23131663+kouryou@users.noreply.github.com>
2020-02-22 19:14:44 +0900

    solve chalange problem of tyr_over3_3

--- 03_method/try_over3_3.rb
+++ 03_method/try_over3_3.rb
@@ -103,16 +103,23 @@ end
 module TryOver3::TaskHelper
   def self.included(klass)
     klass.define_singleton_method :task do |name, &task_block|
-      new_klass = Class.new do
-        define_singleton_method :run do
-          puts "start #{Time.now}"
-          block_return = task_block.call
-          puts "finish #{Time.now}"
-          block_return
-        end
+      define_singleton_method name do
+        puts "start #{Time.now}"
+        block_return = task_block.call
+        puts "finish #{Time.now}"
+        block_return
       end
-      new_klass_name = name.to_s.split("_").map{ |w| w[0] = w[0].upcase; w }.join
-      const_set(new_klass_name, new_klass)
+
+      define_singleton_method(:const_missing) do |const|
+        super unless klass.respond_to?(const.downcase)
+
+        obj = Object.new
+        obj.define_singleton_method :run do
+          warn "Warning: TryOver3::A5Task::#{const}.run is duplicated"
+          klass.send name
+        end
+        obj
+      end
     end
   end
 end
@@ -120,13 +127,6 @@ end
 # TryOver3::TaskHelper は include することで以下のような使い方ができます
 class TryOver3::A5Task
   include TryOver3::TaskHelper
-
-  alias task old_task
-
-  def self.task
-    puts "Warning: #{self} is duplicated"
-    old_task
-  end
 
   task :foo do
     "foo"
